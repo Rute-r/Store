0b191b59ed462ee53d65bdf1678a6c8fdeb4c278
Rute-r <rute_ke@hotmail.com>
2022-11-29 10:55:53 -0300

    Node.Js-Server

---
+++ backend/data.js
@@ -0,0 +1,63 @@
+const data = {
+  products: [
+    {
+      name: 'Old Magic Book',
+      slug: 'old-magic-book',
+      category: 'Magic',
+      image: '/images/image1.jpg',
+      price: 300,
+      countInstock: 2,
+      rating: 5,
+      numReviews: 1,
+      description:
+        'Old Magic Book, only 3 of its kind. Recommended only to witches with more than 10years of experience',
+    },
+    {
+      name: 'Herbal Secrets',
+      slug: 'herbal-secrets-book',
+      category: 'Potions',
+      image: '/images/image2.jpg',
+      price: 50,
+      countInstock: 15,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'This book provide you with new ways to make yout herbal potions. Young witches friendly',
+    },
+    {
+      name: 'How to make potions for beginners',
+      slug: 'how-to-make-potions-book',
+      category: 'Potions',
+      image: '/images/image3.jpg',
+      price: 35,
+      countInstock: 20,
+      rating: 4.1,
+      numReviews: 10,
+      description: 'This book is about potions for beginners',
+    },
+    {
+      name: 'Herbal Secrets: new tricks',
+      slug: 'herbal-secrets-2-book',
+      category: 'Potions',
+      image: '/images/image4.jpg',
+      price: 120,
+      countInstock: 2,
+      rating: 5,
+      numReviews: 1,
+      description: 'More tricks to use your herbals',
+    },
+    {
+      name: 'Magic For Beginners',
+      slug: 'magic-for-beginners-book',
+      category: 'Magic',
+      image: '/images/image5.jpg',
+      price: 120,
+      countInstock: 15,
+      rating: 5,
+      numReviews: 15,
+      description: 'Magic for beginners',
+    },
+  ],
+};
+
+export default data;
